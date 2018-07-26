d9815b1668bcd439c26b13147cf717efa6778e23
Meredith Reece <mardebugger@gmail.com>
2018-07-26 23:27:41 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,21 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :comments
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      category = Category.find_or_create_by(category_attribute)
+      self.categories << category
+    end
+  end
 
+  def username=(username)
+    self.username = User.find_or_create_by(username: username)
+  end
+
+  def username
+    self.username ? self.username : nil
+  end
 end
